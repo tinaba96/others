2499f282b63cf8ff1987aa4de994852b55b0cf51
tinaba96 <tinaba178.96@gmail.com>
2020-08-08 23:59:09 +0900

    update

--- atoms/go2.sh
+++ atoms/go2.sh
@@ -1,9 +1,12 @@
 #! /bin/csh
-#PBS -q SMALL
-#PBS -l select=8:ncpus=20:mpiprocs=20
-#PBS -N p-p239
+#PBS -q SINGLE
+#PBS -l select=1:ncpus=20:mpiprocs=20
+#PBS -N p-p1
 cd ${PBS_O_WORKDIR}
 setenv KMP_AFFINITY disabled
 setenv OMP_NUM_THREADS 1
 
-mpiexec_mpt omplace -nt ${OMP_NUM_THREADS} ../openmx p-p2390.dat -nt ${OMP_NUM_THREADS} > p-p2390.std
+
+mpiexec_mpt omplace -nt ${OMP_NUM_THREADS} ../openmx p-p1$param.dat -nt ${OMP_NUM_THREADS} > p-p1$param.std
+
+
